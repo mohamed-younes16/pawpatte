95e8e6ded99f94710220374e9465755f99d9a7bb
Younes <bettiouim77@gmail.com>
2024-11-02 09:00:03 +0100

    first commit

--- lib/prismabd.ts
+++ lib/prismabd.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-const prismadb = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = prismadb;
-}
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+const prismadb = new PrismaClient().$extends(withAccelerate());
 
-export default prismadb
+// const prismadb = globalThis.prisma || new PrismaClient().$extends(withAccelerate());
+// if (process.env.NODE_ENV !== "production") {
+//   globalThis.prisma = prismadb;
+// }
+
+export default prismadb;
